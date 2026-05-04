718f89eb295b8d1f8d4f5c6d32cc10fd12955d14
prospa <prospa.bongumusa@gmail.com>
2026-05-04 11:13:04 +0200

    db connection and deployment changes

--- InternationalPaymentsAPI/Program.cs
+++ InternationalPaymentsAPI/Program.cs
@@ -6,19 +6,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:4173",
+                "http://127.0.0.1:4173",
+                "https://internationa-payments-app-react-bmfcfsatc2fhb3aa.southafricanorth-01.azurewebsites.net"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "International Payments API is running");
 
 app.Run();
